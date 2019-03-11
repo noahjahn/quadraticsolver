dc23acd26d3e980513da48a9ab6e46e9967e2145
Noah <najahn19@gmail.com>
2019-03-10 20:54:11 -0400

    Added controller file for the program.

---
+++ src/quadratic_solver.c
@@ -0,0 +1,14 @@
+/*
+    Command Line Quadratic Equation Solver
+    JKK Engineering - Kapenga
+    LICENSE: GPL 2.0
+*/
+
+/*
+    Controller for the program. This is where the program starts.
+*/
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
