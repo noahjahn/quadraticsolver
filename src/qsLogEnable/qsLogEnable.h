2941f1ed34eda4a07581f59fade47e67ec01cbc1
Noah <najahn19@gmail.com>
2019-03-10 22:12:03 -0400

    Added macros for enabling logging

--- src/qsLogEnable/qsLogEnable.h
+++ src/qsLogEnable/qsLogEnable.h
@@ -1,1 +1,12 @@
-int qsLogEnable(...);
+/*
+    Command Line Quadratic Equation Solver
+    TEAM: Noah Jahn, Paul VanderWeele
+    JKK Engineering - Kapenga
+    LICENSE: GPL 2.0
+*/
+
+/*
+    Enable logging through out the program with a simple macro
+*/
+
+int qsLogEnable();
