29df8f64818e20ba29625b6b93ba4116a76a3bf7
noahjahn <najahn19@gmail.com>
2019-03-26 18:24:20 -0400

    Added description.

--- src/qsLogEnable/qsLogEnable.c
+++ src/qsLogEnable/qsLogEnable.c
@@ -8,7 +8,12 @@ Enables logging throughout the system.
 
 **Input**
 
+The address of the global logging integer
+
 **Output**
+
+Changes the global logging integer to 1 for logging on.
+
 Returns whether an error occurred or not.
 0 	- Successful
 -1 	- Unsuccessful
