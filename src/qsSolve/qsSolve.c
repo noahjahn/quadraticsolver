ec24740f8edc3ba7f57baffe1eacc63b6f1d553f
noahjahn <najahn19@gmail.com>
2019-03-25 20:52:29 -0400

    Added math and successful returns

--- src/qsSolve/qsSolve.c
+++ src/qsSolve/qsSolve.c
@@ -10,16 +10,74 @@ Determines what the roots of the quadratic means.
 
 **Output**
 Returns whether an error occurred or not.
-0 	- Successful
--1 	- Unsuccessful
-2+ 	- Undefined
+-3 	- argument out of range
+-2 	- NULL a argument
+-1 	- Internal error
+2	- No real roots
+3	- One real root
+4	- Two real roots
+5+ 	- Undefined
 *****************************************************************/
 
+#include <math.h>
 #include "qsSolve.h"
+#include "../qsLog/qsLog.h"
 
 int qsSolve(double a, double b, double c, double *x1, double *x2) {
 	int error = 0; //Successful
-	//Validate input
+
+	if (logging) {
+		qsLog("qsSolve.c - qsSolve - Entered new function, argument passed in: a = %f, b = %f, c = %f, *x1 = %p, *x2 = %p\n", a, b, c, x1, x2);
+	}
+
+	// Validate input
+	if (NULL == a) {
+		if (logging) {
+			qsLog("qsSolve.c - qsSolve - Argument a is NULL\n");
+		}
+		error = -2;
+	} else if (a > (3.4 * pow(10,38)) || a < (-3.4 * pow(10,38))) {
+		if (logging) {
+			qsLog("qsSolve.c - qsSolve - Argument a is out of range\n");
+		}
+		error = -3;
+	}
+		if (NULL == b) {
+			error = -5;
+		} else if (b > (3.4 * pow(10,38))) {
+			error = -6;
+		} else if (b < (-3.4 * pow(10,38))) {
+			error = -7;
+		}
+
+	}
+
+	// reset x1 and x2, they shouldn't have anything stored in them
+	*(x1) = NULL;
+	*(x2) = NULL;
+
+
+	double radicand = (b * b) - (4 * a * c);
+
+	if (radicand < 0) { // then we have a complex number. No real roots
+		if (logging) {
+			qsLog("qsSolve.c - qsSolve - No real roots determined\n");
+		}
+		error = 2; // do nothing else
+	} else if (radicand == 0) { // one real root
+		if (logging) {
+			qsLog("qsSolve.c - qsSolve - One real root determined\n");
+		}
+		error = 3; // we need to return that the solution has one real root
+		*(x1) = -b / (2 * a);
+	} else { // two real roots
+		if (logging) {
+			qsLog("qsSolve.c - qsSolve - Two real roots determined\n");
+		}
+		error = 4; // we need to return that the solution has two real roots
+		*(x1) = (-b + sqrt(radicand)) / (2 * a);
+		*(x2) = (-b - sqrt(radicand)) / (2 * a);
+	}
 
 	return error;
 }
