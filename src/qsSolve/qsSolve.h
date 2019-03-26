1b96f94a97dd33f5bf62a66dd21861484df83ae5
noahjahn <najahn19@gmail.com>
2019-03-26 18:25:40 -0400

    Updated heading comment

--- src/qsSolve/qsSolve.h
+++ src/qsSolve/qsSolve.h
@@ -10,5 +10,5 @@ qsSolve() 	- Calculates quadratic roots from coefficients.
 tqsSolve()	- Tests qsSolve and reports a failure.
 **************************************************************/
 
-int qsSolve(double a, double b, double c, double *x1, double *x2);
+int qsSolve(double a, double b, double c, double * x1, double * x2);
 int tqsSolve();
